90a410e6950cec11ba9fa49bba2d954a82672d4d
Stanley Dudek <StanDudek@GMail.com>
2016-11-12 23:12:27 -0600

    Update motionemailer.py
    Added confirmations of file identifications

--- motionemailer.py
+++ motionemailer.py
@@ -1,5 +1,4 @@
-import os,glob
-
+import os,glob,time
 #these are identical because I have no idea what I'm doing
 def current():
 	current = max(glob.iglob(os.path.join('/media/pi/EXTSD/Security','*.jpg')),key=os.path.getctime)
@@ -7,3 +6,14 @@ def current():
 def newest():
 	newest = max(glob.iglob(os.path.join('/media/pi/EXTSD/Security','*.jpg')),key=os.path.getctime)
 	return newest
+#basic identification confirmation, no doubt an awful way to do this
+while True:
+	cur=current()
+	while True:
+		if new==current:
+			time.sleep(0.1)
+			print('these files are identical')
+		else:
+			time.sleep(0.1)
+			print('these files are different')
+			break
